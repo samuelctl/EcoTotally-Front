6e850f4d0a19b8d1744fdffbbf9be88e02e6b427
samuelctl <samuel.s.aguiar@aluno.senai.br>
2026-05-04 12:20:38 -0300

    Correção de erros

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ecototally.app',
   appName: 'EcoTotally',
-  webDir: 'dist'
+  webDir: 'dist/ecototally/browser'
 };
 
 export default config;
